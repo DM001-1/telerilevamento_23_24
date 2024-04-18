704e8a15e1ee5e3361b2c721205c6a8df0dfd49f
DM001-1 <136607597+DM001-1@users.noreply.github.com>
2024-04-18 18:42:34 +0200

    Create installazione_pacchetti.R

---
+++ installazione_pacchetti.R
@@ -0,0 +1,11 @@
+# install.packages("terra")
+# install.packages("devtools")
+
+library(terra) # or require()
+library(devtools)
+
+# install the imageRy package from GitHub
+# a devtools function:
+# install_github("ducciorocchini/imageRy")
+
+library(imageRy)
